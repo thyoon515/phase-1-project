7ef7436a4736e26eb048477dd1c095f1dab5dbc2
thyoon515 <thyoon515@gmail.com>
2022-05-27 22:51:20 -0400

    Event listener, event handler and helper change const arrow function to function. Nodes change const function to const arrow function

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -3,7 +3,6 @@
 
 /** Nodes **/
 const mainDiv = () => document.getElementById('main');
-//set variables as function to lower bug issues
 const homeLink = () => document.getElementById('home-link');
 const searchByNameLink = () => document.getElementById('search-by-name-link');
 const searchByIngredientLink = () => document.getElementById('search-by-ingredient-link');
@@ -11,73 +10,73 @@ const surpriseMeLink = () => document.getElementById('surprise-me');
 
 /** Event Listeners **/
 //3 question rules: When? Cause? Effect?
-const homePageLinkEvent = () => {
-    homeLink().addEventListener('click', loadHomePage)
+function homePageLinkEvent(){
+    homeLink().addEventListener('click', renderHomePage)
 }
-const searchByNameLinkEvent = () => {
-    searchByNameLink().addEventListener('click', loadSearchByName)
+function searchByNameLinkEvent(){
+    searchByNameLink().addEventListener('click', renderSearchByName)
 }
-const searchByIngredientLinkEvent = () => {
-    searchByIngredientLink().addEventListener('click', loadSearchByIngredient)
+function searchByIngredientLinkEvent(){
+    searchByIngredientLink().addEventListener('click', renderSearchByIngredient)
 }
-const surpriseMeLinkEvent = () => {
-    surpriseMeLink().addEventListener('click', fetchSurpriseCocktail)
-}
+// function surpriseMeLinkEvent(){
+//     surpriseMeLink().addEventListener('click', fetchSurpriseCocktail())
+// }
 
 /** Event Handlers **/
-const loadHomePage = () =>{
+function renderHomePage(){
     resetMainDiv();
-    //resetting the page everytime it's loads
+    //reset the page everytime it renders
     const h1 = document.createElement('h1');
     h1.className = 'center-align';
     h1.innerText = "Welcome to Automated Cocktail Bar";
     mainDiv().appendChild(h1);
 }
 
-const loadSearchByName = event => {
-    event.preventDefault();
+function renderSearchByName(){
+    
     resetMainDiv();
     const h1 = document.createElement('h1');
     h1.innerText = 'Search Cocktail Recipe by Name';
     mainDiv().appendChild(h1);
 }
 
-const loadSearchByIngredient = event => {
-    event.preventDefault();
+function renderSearchByIngredient(){
+    
     resetMainDiv();
     const h1 = document.createElement('h1');
     h1.innerText = 'Search Cocktail Recipe by Ingredients'
     mainDiv().appendChild(h1);
 }
 
-function loadSurpriseMe(surprise){
+function renderSurpriseMe(){
     resetMainDiv();
     const h1 = document.createElement('h1');
     h1.innerText = 'SURPRISE!!'
     mainDiv().appendChild(h1);
 }
 
-const fetchSurpriseCocktail = () => {
-    fetch("http://www.thecocktaildb.com/api/json/v1/1/random.php")
-        .then(resp => resp.json())
-        .then(console.log(data));
-        }
+// function fetchSurpriseCocktail(){
+//     fetch("http://www.thecocktaildb.com/api/json/v1/1/random.php")
+//         .then(resp => resp.json())
+//         .then(console.log(data));
+//         }
 
 
 
 /** Helper **/
-const resetMainDiv = () => {
-    mainDiv().innerHTML= '';
+function resetMainDiv(){
+    mainDiv().innerHTML= ""
 //instead of adding, this will reset the page
 }
 
 
 /** Startup **/
 
-document.addEventListener('DOMContentLoaded', function(){
-    loadHomePage();
+document.addEventListener('DOMContentLoaded', () => {
+    renderHomePage();
     homePageLinkEvent();
     searchByNameLinkEvent();
     searchByIngredientLinkEvent();
-    surpriseMeLinkEvent();
+    //surpriseMeLinkEvent();
 })
