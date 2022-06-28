181fd1a99a3e4e03ceb3a19162eb597d4c7bb2f0
thyoon515 <thyoon515@gmail.com>
2022-06-27 22:01:29 -0400

    Update variable name more specific for each function

--- JavaScripts/index.js
+++ JavaScripts/index.js
@@ -21,84 +21,82 @@ function surpriseMeLinkEvent(){
 
 function renderHomePage(){
     resetMainDiv(); //reset the page everytime it renders
-    const h1 = document.createElement('h1');
-    const img = document.createElement('img');
-    h1.className = 'center-align';
-    h1.innerText = "Welcome to Cocktail Recipe";
-    img.src = 'https://customneon.com/media/catalog/product/cache/1d858328874ebd6a1883e32a918ffc61/c/o/cocktail-glass-turnedon-customneon.jpg'
-    img.setAttribute('style', 'margin-top: 100px')
-    mainDiv().appendChild(h1);
-    h1.appendChild(img);
+    const homePageHeader = document.createElement('h1');
+    const homePageImage = document.createElement('img');
+    homePageHeader.className = 'center-align';
+    homePageHeader.innerText = "Welcome to Cocktail Recipe";
+    homePageImage.src = 'https://customneon.com/media/catalog/product/cache/1d858328874ebd6a1883e32a918ffc61/c/o/cocktail-glass-turnedon-customneon.jpg'
+    homePageImage.setAttribute('style', 'margin-top: 100px')
+    mainDiv().appendChild(homePageHeader);
+    homePageHeader.appendChild(homePageImage);
 }
 
 function renderSearchByName(){
     resetMainDiv();
-    const h1 = document.createElement('h1');
-    const form = document.createElement('form');
-    const div1 = document.createElement('div');
-    const div2 = document.createElement('div');
-    const input = document.createElement('input');
-    const label = document.createElement('label');
-    const btn = document.createElement('button');
-    h1.className = 'center-align';
-    form.className = 'col s12';
-    div1.className = 'row container';
-    div2.className = 'input-field col s6';
-    input.className = 'validate';
-    btn.className = 'waves-effect waves-light btn'
-    form.id = 'search-by-name-form';
-    input.id = "text-input";
-    btn.id = "search-by-name-submit";
-    input.setAttribute('type', 'text');
-    label.setAttribute('for', 'Cocktail_Name');
-    form.setAttribute('action', '#');
-    form.setAttribute('method', 'POST');
-    h1.innerText = 'Search Cocktail Recipe by Name';
-    label.innerText = 'Cocktail Name';
-    btn.innerText = 'search'; 
+    const searchByNameHeader = document.createElement('h1');
+    const searchForm = document.createElement('form');
+    const rowContainerDiv = document.createElement('div');
+    const inputFieldDiv = document.createElement('div');
+    const formInput = document.createElement('input');
+    const inputLabel = document.createElement('label');
+    const searchButton = document.createElement('button');
+    searchByNameHeader.className = 'center-align';
+    searchForm.className = 'col s12';
+    rowContainerDiv.className = 'row container';
+    inputFieldDiv.className = 'input-field col s6';
+    formInput.className = 'validate';
+    searchButton.className = 'waves-effect waves-light btn'
+    searchForm.id = 'search-by-name-form';
+    formInput.id = "text-input";
+    searchButton.id = "search-by-name-submit";
+    formInput.setAttribute('type', 'text');
+    inputLabel.setAttribute('for', 'Cocktail_Name');
+    searchForm.setAttribute('action', '#');
+    searchForm.setAttribute('method', 'POST');
+    searchByNameHeader.innerText = 'Search Cocktail Recipe by Name';
+    inputLabel.innerText = 'Cocktail Name';
+    searchButton.innerText = 'search'; 
     
-    form.addEventListener('submit', (e) =>{
+    searchForm.addEventListener('submit', (e) =>{
         e.preventDefault();
         fetchSearchByName(e.target[0].value); //target input field to pass in input value to search the API
-        form.reset();
+        searchForm.reset();
     })
 
-    mainDiv().appendChild(h1);
-    mainDiv().appendChild(form)
-    form.appendChild(div1);
-    div1.appendChild(div2);
-    div2.appendChild(input);
-    div2.appendChild(label);
-    div2.appendChild(btn);
+    mainDiv().append(searchByNameHeader, searchForm);
+    
+    searchForm.appendChild(rowContainerDiv);
+    rowContainerDiv.appendChild(inputFieldDiv);
+    inputFieldDiv.append(formInput, inputLabel, searchButton);
 }    
    
 
 function renderSurpriseMe(cocktail){
     resetMainDiv();
-    const h1 = document.createElement('h1');
-    const div = document.createElement('div');
-    const img = document.createElement('img'); 
-    const h4Recipe = document.createElement('h4');
-    const h4Instruction = document.createElement('h4');
-    const ul = document.createElement('ul');
-    div.className = 'card';
-    h1.innerText = 'SURPRISE!!';
-    h4Recipe.innerText = cocktail.drinks[0].strDrink;
-    h4Instruction.innerText = cocktail.drinks[0].strInstructions
-    img.src = cocktail.drinks[0].strDrinkThumb;
+    const supriseHeader = document.createElement('h1');
+    const cardDiv = document.createElement('div');
+    const cocktailImage = document.createElement('img'); 
+    const cocktailRecipe = document.createElement('h4');
+    const cocktailInstruction = document.createElement('h4');
+    const recipeUl = document.createElement('ul');
+    cardDiv.className = 'card';
+    supriseHeader.innerText = 'SURPRISE!!';
+    cocktailRecipe.innerText = cocktail.drinks[0].strDrink;
+    cocktailInstruction.innerText = cocktail.drinks[0].strInstructions
+    cocktailImage.src = cocktail.drinks[0].strDrinkThumb;
 
     for(let i=1; i<16; i++){
         //iterate through each ingredients and measurements
         if(cocktail.drinks[0][`strIngredient${i}`] === null | cocktail.drinks[0][`strIngredient${i}`] === ""){
             break;
         }
-        let li = document.createElement('li');
-        li.innerHTML = cocktail.drinks[0][`strIngredient${i}`] + ': ' + cocktail.drinks[0][`strMeasure${i}`]
-        ul.appendChild(li);
+        let ingredientMeasurementList = document.createElement('li');
+        ingredientMeasurementList.innerHTML = cocktail.drinks[0][`strIngredient${i}`] + ': ' + cocktail.drinks[0][`strMeasure${i}`]
+        recipeUl.appendChild(ingredientMeasurementList);
     }
-    mainDiv().append(h1, div);
-    div.append(img, h4Recipe, h4Instruction);
-    h4Recipe.append(ul);  
+    mainDiv().append(supriseHeader, cardDiv);
+    cardDiv.append(cocktailImage, cocktailRecipe, cocktailInstruction);
+    cocktailRecipe.append(recipeUl);  
 }
 
 function fetchSurpriseMe(){
@@ -116,15 +114,15 @@ function fetchSearchByName(cocktail){
 
         data.drinks.forEach(drink => {
             const cardDiv = document.createElement('div');
-            const img = document.createElement('img');
-            const h3 = document.createElement('h3');
-            const h4Ins = document.createElement('h4');
-            const h4 = document.createElement('h4');
-            const ul = document.createElement('ul');
+            const cocktailImage = document.createElement('img');
+            const cocktailName = document.createElement('h3');
+            const cocktailInstruction = document.createElement('h4');
+            const ingredientMeasurementHeader = document.createElement('h4');
+            const ingredientMeasurementUl = document.createElement('ul');
             cardDiv.className = 'card';
-            img.src = drink.strDrinkThumb;
-            h3.innerText = drink.strDrink;
-            h4Ins.innerText = drink.strInstructions;
+            cocktailImage.src = drink.strDrinkThumb;
+            cocktailName.innerText = drink.strDrink;
+            cocktailInstruction.innerText = drink.strInstructions;
 
             for(let i=1; i<16; i++){
                 if(drink[`strIngredient${i}`] === null | drink[`strIngredient${i}`] === ""){
@@ -132,11 +130,11 @@ function fetchSearchByName(cocktail){
                 }
                 let li = document.createElement('li');
                 li.innerHTML = drink[`strIngredient${i}`] + ': ' + drink[`strMeasure${i}`]
-             ul.appendChild(li);
+                ingredientMeasurementUl.appendChild(li);
             }
 
-            h4.appendChild(ul);
-            cardDiv.append(h3, img, h4, h4Ins);
+            ingredientMeasurementHeader.appendChild(ingredientMeasurementUl);
+            cardDiv.append(cocktailName, cocktailImage, ingredientMeasurementHeader, cocktailInstruction);
             div.appendChild(cardDiv);
         })
         mainDiv().append(div);
